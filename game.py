0c0a1cf763add2a5195a190f88d38e3a305927a2
root <root@ansible.ziyotek.local>
2020-04-04 02:39:29 -0400

    added new changes

--- game.py
+++ game.py
@@ -1,3 +1,23 @@
 # Author: bahadir
 # Contact: bahadirfayzi@hotmail.com
 # Fun Game
+name = input ("what is your name?")
+age = int (input ("what is your age ?"))
+your_age_in_hundred_years = age+100
+likes_pinaapple = True
+
+
+
+print("Hi, " + name + "!")
+print("Your age is " + str(age) + " but in 100 years, you are going to be " + str(your_age_in_hundred_years))
+
+if (likes_pinaapple):
+    print("Hello")
+if (likes_pinaapple == True):
+    print ("Hello")
+    
+
+
+
+
+
